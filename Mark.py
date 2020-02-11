318573f73c44b776551d9ba943d249d75ba98fa1
17721250181 <60857118+17721250181@users.noreply.github.com>
2020-02-11 15:04:09 +1300

    Create Mark.py

---
+++ Mark.py
@@ -0,0 +1,10 @@
+mark = float(input("What is your grade?"))
+
+if mark >= 90:
+    print ( "A" )
+if mark >= 70 and mark < 90:
+    print ( "B" )    
+if mark >= 50 and mark < 70:
+    print ( "C" )
+if mark < 50:
+    print ( "Fail" )
